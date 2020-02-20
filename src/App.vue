657dc0f5a6e582697fa04d4ca7d3e08e153a3465
Jiahe Chen <jiahe_chen@apple.com>
2020-02-19 21:01:28 -0600

    test

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+
+<template><vn-alert> 
+  <div>Case contains sensitive health information</div>
+</vn-alert></template>
+<template>
+  
+</template>
+
+<script>
+import VnAlert from './components/VnAlert.vue'
+import VnClosablePanel from './components/VnClosablePanel.vue'
+
+export default {
+  name: 'app', 
+  components: {
+    VnAlert
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VnAlert.vue
@@ -0,0 +1,32 @@
+<template>
+  
+    <div class="father">
+      <slot></slot>
+    </div>
+   
+</template>
+
+
+
+
+<script>
+export default { 
+  name : 'VnAlert',
+  
+};
+</script>
+
+<style scoped>
+
+.father {
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  color: white;
+  background-color:#2556c0;
+  padding: 1%; 
+  margin: -10px ;
+  
+}
+</style>
+
